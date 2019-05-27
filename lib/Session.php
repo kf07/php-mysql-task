8be03f0a3ef174561cc852ae2f519e980e9f2471
kango furusu <furusu0715@gmail.com>
2019-05-28 00:39:49 +0900

    Session.php作成

--- lib/Session.php
+++ lib/Session.php
@@ -1,1 +1,23 @@
 <?php
+class Session
+{
+  protected $bag;
+
+  public
+  function __construct($namespace = 'app')
+  {
+    if (!session_id()) {
+      session_start();
+    }
+
+    $this->bag = &$_SESSION[$namespace];
+
+    if (!isset($this->bag)) {
+      $this->bag[$this->getAppDataKey()] = [];
+      if (!$this->getCsrfToken()) {
+        $this->bag[$this->getCsrfTokenKey()] = $this->generateCsrfToken();
+      }
+    }
+  }
+
+}
